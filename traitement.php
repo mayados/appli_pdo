0b28f65d707e2e9ad5961ba976c3c36db6c153b2
mayados <mariannedos.cours@gmail.com>
2022-10-26 11:03:39 +0200

    get different elements of the product add

--- traitement.php
+++ traitement.php
@@ -2,32 +2,40 @@
     /* Permet de démarrer une session sur le serveur pour l'utilisateur courant, ou la récupérer s'il en avait déjà une */
     session_start();
     require('functions.php');
+    require('db-functions.php');
+    connexion();
+    findAll();
 
     /* On a indiqué dans index que le mot clé pour récupérer action s'appelle "action"*/
     $action = $_GET["action"];
     /* Pour éviter des erreurs, on vérifie s'il y a bien la valeur à la variable ref / les  ":" signifient que s'il n'y a pas on met rien */
     $ref = (isset($_GET['ref'])) ? $_GET['ref'] : "";
+    $name = (isset($_GET['name'])) ? $_GET['name'] : "";
+    $price = (isset($_GET['price'])) ? $_GET['price'] : "";
+    var_dump($ref);
     $produit = (isset($_GET['produit'])) ? $_GET['produit'] : "";    
     /* Si un produit est ajouté.. */
     switch($action) {
 
         case "ajouterProduit":
-              /* Vérifier si la clé "submit" correspond bien à l'attribut "name" du bouton du formulaire : cela limite l'accès à traitement.php. Seules les requêtes HTTP provenant de la soumission de notre formulairesont acceptées */
-            if(isset($_POST['submit'])){
+              /* Vérifier si l'on a la clé id du produit */
+            if(isset($_GET['ref'])){
                 /* Vérification de l'intégralité des valeurs transmises dans le tableau $_POST en fonction de celles que nous attendons */
-                $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
-                $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-                $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+                $id = $ref;
+                $name = $name;
+                $price = $price;
+                // $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
 
                 //Nous devons conserver chaque produit renseigné, donc les stocker esession. On décide d'abord de leur organisation au sein de la session 
-                if($name && $price && $qtt){
+                if($name){
 
                     $product = [
+                        "id" => $id,
                         "name" => $name,
-                        "price" => $price,
-                        "qtt" => $qtt,
-                        "total" => $price*$qtt
+                        "price" => $price
+                        // "qtt" => $qtt,
+                        // "total" => $price*$qtt
                     ];
                 
                     /* On enregistre le produit en session */
@@ -41,8 +49,8 @@
             }
 
 
-            /* Redirection vers le formulaire, qu'il soit saisi ou non */
-            header("Location:index.php");
+            /* Redirection vers la page admin, qu'il soit saisi ou non */
+            header("Location:recap.php");
         break;
 
         /* On a nommé l'action "viderPanier" dans récap.php. Au cas où c'est cela, on retire tous les produits de la session et on rediirige vers la page recap.php */
