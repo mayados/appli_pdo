2a66f1a2a6a755f031c4f83fda0ceab91f202f37
mayados <mariannedos.cours@gmail.com>
2022-10-27 14:57:13 +0200

    call to function findOneById to get the elements

--- product.php
+++ product.php
@@ -17,20 +17,17 @@
         <a href="index.php">Retour</a>
         <?php
             /* Nous récupérons la valeur envoyée dans le lien, qui correspond à l'id du produit */
-            $ref = (isset($_GET['ref'])) ? $_GET['ref'] : "";
-            $produit = findOneById($ref);
+            $produit = findOneById($id);
 
 
-            foreach ($produit as $prod) {
-                echo "<br>".$prod['name']."<br>"
-                .$prod['description']."<br>"
-                .$prod['price']." euros<br>"; 
+                echo "<br>".$produit['name']."<br>"
+                .$produit['description']."<br>"
+                .$produit['price']." euros<br>"; 
                 echo "<a href='traitement.php?action=ajouterProduit&ref=".$id."'>Ajouter au panier</a>";        
-            }
+
             // die;
         ?>
         <!-- On renvoie vers la page traitement pour que les produits soient ajoutés -->
-        
     </div>
 </body>
 </html>
