4d48860f549deea02f0d472765bb8b393ab8ef73
mayados <mariannedos.cours@gmail.com>
2022-10-27 10:48:45 +0200

    delete code lines about the prodcuts shown

--- admin.php
+++ admin.php
@@ -13,9 +13,6 @@
 
     /* Il faut avoir accès au tableau $_SESSION pour connaître le nombre de produits actuels */
     session_start();
-    require('db-functions.php');
-    connexion();
-    findAll();
     require('functions.php');
     showMessage();
     ?>
