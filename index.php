1f6db5efab0c94e380d9413fa88b1276e58cd22e
mayados <mariannedos.cours@gmail.com>
2022-10-27 10:47:55 +0200

    recreate index php page for the user

--- index.php
+++ index.php
@@ -5,37 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="refresh" content="5"> -->
-    <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/styles.css"> -->
     <title>index php</title>
 </head>
 <body>
     <div id="container-page">
     <h1>Ajouter un nouveau produit</h1>
-        <section id="formulaire">
-            <form action="traitement.php?action=ajouterProduit" method="post">
-                <p>
-                    <label>
-                        <p class="label-p">Nom du produit :</p>
-                        <input required type="text" name="name">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <p class="label-p">Prix du produit :</p>
-                        <input required type="number" step="any" name="price" min=0>
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <p class="label-p">Quantité désirée :</p>
-                        <input required type="number" name="qtt" value="1" min=1>
-                    </label>
-                </p>
-                <p>
-                    <input id="ajouter" type="submit" name="submit" value="Ajouter le produit">
-                </p>
-                <p>
-                    <label>
+    <?php
+        /* Il faut avoir accès au tableau $_SESSION pour connaître le nombre de produits actuels */
+        session_start();
+        require('db-functions.php');
+        connexion();
+        findAll();
+        require('functions.php');
+        showMessage();
+        
+    ?>
+
                         <p>Nombre de produits actuels : 
                             <?php
                                
@@ -47,13 +33,7 @@
                                     echo "0";
                                 }
                             ?>
-                        </p>
-                    </label>
-                </p>
-            </form>
-        </section>
-
-        <a id="recap" href="recap.php">Récapitulatif</a>            
+          
         
 
     </div>
