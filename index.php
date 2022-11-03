45d5156c22fd0e51907a305e4ea34515edd4fea5
mayados <mariannedos.cours@gmail.com>
2022-11-03 16:44:18 +0100

    add div + id + class

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+            /* Il faut avoir accès au tableau $_SESSION pour connaître le nombre de produits actuels */
+            session_start();
+            require('db-functions.php');
+            connexion();
+            /* On utilise findAll() pour trouver chaque produit */
+            $products = findAll();
+            require('functions.php');
+            showMessage();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,44 +15,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta http-equiv="refresh" content="5"> -->
-    <!-- <link rel="stylesheet" href="css/styles.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <link rel="stylesheet" href="css/index.css">
     <title>index php</title>
 </head>
 <body>
     <div id="container-page">
-    <?php
-        /* Il faut avoir accès au tableau $_SESSION pour connaître le nombre de produits actuels */
-        session_start();
-        require('db-functions.php');
-        connexion();
-        /* On utilise findAll() pour trouver chaque produit */
-        $products = findAll();
-        require('functions.php');
-        showMessage();
-        
-        /* Pour chaque produit, on affiche ses informations */
-        foreach ($products as $product) {
-        echo "<br><br><a href='product.php?ref=".$product['id']."'>".$product['name']."</a><br><br> "
-        .substr($product['description'],0,45)."...<br> 
-        <strong>".$product['price']." euros</strong><br>
-        <a href='traitement.php?action=ajouterProduit&ref=".$product['id']."'>Ajouter au panier</a>"; 
-        }
-    ?>
-
-                        <p>Nombre de produits actuels : 
-                            <?php
-                               
-                                if(isset($_SESSION['products'])){
-                                    /* Déclarer la variable ici et non en dehors de la condition, sinon cela fera undefined */
-                                    $nombreProduits = $_SESSION['products']; 
-                                    echo " ".count($nombreProduits)."<br>";
-                                } else{
-                                    echo "0 <br>";
-                                }
-                            ?>
-
-        <a id="recap" href="recap.php">Récapitulatif</a> 
-          
+        <div id="container-infos">
+            <div id="panier">
+                <a id="recap" href="recap.php"><i class="fa-solid fa-cart-shopping"></i></a> 
+                <p id="nb-produits">
+                                    <?php
+                                    
+                                        if(isset($_SESSION['products'])){
+                                            /* Déclarer la variable ici et non en dehors de la condition, sinon cela fera undefined */
+                                            $nombreProduits = $_SESSION['products']; 
+                                            echo " ".count($nombreProduits)."";
+                                        } else{
+                                            echo "0";
+                                        }
+                                    ?>
+                </p>
+            </div>
+        </div>
+        <div id="container-products">
+            <?php
+            
+            /* Pour chaque produit, on affiche ses informations */
+            foreach ($products as $product) {
+            echo "<div class ='container-product'>
+                    <h3><a href='product.php?ref=".$product['id']."'>".$product['name']."</a></h3> "
+                    .substr($product['description'],0,45)."...<br> 
+                    <strong>".$product['price']." euros</strong><br>
+                    <a href='traitement.php?action=ajouterProduit&ref=".$product['id']."'>Ajouter au panier</a>
+                </div>"; 
+            }
+        ?>
+        </div>
         
 
     </div>
